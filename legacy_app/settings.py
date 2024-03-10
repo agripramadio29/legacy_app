fef3d6b2a31bd16135caaa7e0ef310b064e9ec49
agripramadio29 <businessagri29@gmail.com>
2024-03-10 19:51:59 +0700

    Update settings.py

--- legacy_app/settings.py
+++ legacy_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&%4oxrb1a1od$o)18i0amas@@q09=jc2^v-22gft95@8i#pq01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
