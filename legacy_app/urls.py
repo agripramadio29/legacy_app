b589480759299a3aa7754c4300a46a208616e2b6
agripramadio29 <businessagri29@gmail.com>
2024-03-17 14:29:12 +0700

    clarity url

--- legacy_app/urls.py
+++ legacy_app/urls.py
@@ -15,21 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view, pg_stop, pg_start, pg_restart, about, at_start, at_stop, at_restart
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
+    path('', views.home_view, name='home'),
 
     #services
-    path('pg_stop/', pg_stop, name='pg_stop'),
-    path('pg_start/', pg_start, name='pg_start'),
-    path('pg_restart/', pg_restart, name='pg_restart'),
-    path('at_start/', at_start, name='at_start'),
-    path('at_stop/', at_stop, name='at_stop'),
-    path('at_restart/', at_restart, name='at_restart'),
+    path('pg_stop/', views.pg_stop, name='pg_stop'),
+    path('pg_start/', views.pg_start, name='pg_start'),
+    path('pg_restart/', views.pg_restart, name='pg_restart'),
+    path('at_start/', views.at_start, name='at_start'),
+    path('at_stop/', views.at_stop, name='at_stop'),
+    path('at_restart/', views.at_restart, name='at_restart'),
 
 
-    path('about/', about, name="about")
+    path('about/', views.about, name="about")
 
 ]
